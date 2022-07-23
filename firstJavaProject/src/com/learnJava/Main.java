64e5cfd932588455551b2ee0345c38429e3fc555
lina-22 <haquefahinur@gmail.com>
2022-07-23 08:28:16 +0200

    Java practicing oparators

--- firstJavaProject/src/com/learnJava/Main.java
+++ firstJavaProject/src/com/learnJava/Main.java
@@ -3,7 +3,8 @@ package com.learnJava;
 public class Main {
 
     public static void main(String[] args){
-          System.out.println("My first Java Project");
+           int age = 30;
+          System.out.println(age);
     }
 }
 
